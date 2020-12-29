27d60777b844ad3c3cfccd9dfa4d79237b1fa924
taltatal <32167238+taltatal@users.noreply.github.com>
2020-12-29 16:00:22 +0200

    working and niuce looing

--- single_led_detector.py
+++ single_led_detector.py
@@ -16,6 +16,7 @@ stdv = 30
 
 class ImageProcessing:
     INTERPOLATION = cv2.INTER_CUBIC
+    STDV = 50
 
     def __init__(self, camera):
         self.camera = camera
@@ -24,9 +25,15 @@ class ImageProcessing:
         self.window.open()
         self.pipeline = None
         self.bbox_bound = None
+        self.bbox_pipeline = None
         self.current_original_frame = None
         self.current_cropped_frame = None
         self.current_piped_frame = None
+
+    def setup(self):
+        self.crop_init()
+        self.init_pipeline()
+        cv2.destroyAllWindows()
 
     def camera_init(self):
         self.camera.set_exposure(-5)
@@ -38,10 +45,13 @@ class ImageProcessing:
 
     def init_pipeline(self, *temp_args):
         # pipeline = gbv.ColorThreshold([[0, 100], [0, 255], [0, 255]], 'HSV')
-        pipeline = gbv.median_threshold(*temp_args)
-        print(pipeline)
+
+        self.get_cropped_frame()
+        bbox = cv2.selectROI('feed', self.current_cropped_frame)
+        self.pipeline = gbv.median_threshold(self.current_cropped_frame, ImageProcessing.STDV, bbox, 'RGB')
+        print(self.pipeline)
         # pipeline += gbv.ErodeAndDilate(2)
-        return pipeline
+        return self.pipeline
 
     def get_original_frame(self):
         ok, self.current_original_frame = self.camera.read()
@@ -53,7 +63,7 @@ class ImageProcessing:
         return self.current_cropped_frame
 
     def get_piped_frame(self):
-        return get_pipeline()(self.current_cropped_frame)
+        return self.pipeline(self.current_cropped_frame)
 
     def get_frame(self):
         self.get_original_frame()
@@ -61,86 +71,25 @@ class ImageProcessing:
         return self.get_piped_frame()
 
 
-
-def get_pipelined_window_stream(pipeline):
-    return gbv.FeedWindow(window_name='after threshold', drawing_pipeline=pipeline)
-
-
-def show_pipelined_video(frame, pipeline):
-    return get_pipelined_window_stream(pipeline).show_frame(frame)
-
-
-def get_pipeline(*temp_args):
-    # pipeline = gbv.ColorThreshold([[0, 100], [0, 255], [0, 255]], 'HSV')
-    pipeline = gbv.median_threshold(*temp_args)
-    print(pipeline)
-    # pipeline += gbv.ErodeAndDilate(2)
-    return pipeline
-
-
-def get_hidden_camera():
-    return
-
-
 def main():
     # camera = gbv.USBCamera(0)
     camera = gbv.USBCamera(r"C:\Users\t8854535\Desktop\sprint2\test_data\record.avi")
     imgp = ImageProcessing(camera)
-    imgp.crop_init()
-    imgp.init_pipeline()
-
-
-def main2():
-    # camera = gbv.USBCamera(0)
-    camera = gbv.USBCamera(r"C:\Users\t8854535\Desktop\sprint2\test_data\green_led.avi")
-    camera.set_exposure(-5)
-    camera.resize(.1, .1)
-    window = gbv.CameraWindow('feed', camera)
-    window.open()
-
-    frame = window.show_and_get_frame()
-    bbox_bound = cv2.selectROI('feed', frame)
-
-    interpol = cv2.INTER_CUBIC
-
-    frame = gbv.crop(window.show_and_get_frame(), *bbox_bound)
-    frame = cv2.resize(frame, (1000, 1000), interpolation=interpol)
-
-    cv2.destroyAllWindows()
+    imgp.setup()
 
     original = gbv.FeedWindow(window_name='original')
-
-    while True:
-        ok, frame = camera.read()
-        frame = gbv.crop(frame, *bbox_bound)
-        frame = cv2.resize(frame, (800, 800), interpolation=interpol)
-        original.show_frame(frame)
-        k = original.last_key_pressed
-        if k == 'r':
-            bbox = cv2.selectROI('original', frame)
-            pipeline = get_pipeline(frame, stdv, bbox, 'RGB')
-            # thr = gbv.ColorThreshold([[0,100], [0,255], [0,255]], 'HSV')
-            break
-    cv2.destroyAllWindows()
-
-    print(pipeline)
-
-    original = gbv.FeedWindow(window_name='original')
-    after_proc = gbv.FeedWindow(window_name='after threshold', drawing_pipeline=pipeline)
+    after_proc = gbv.FeedWindow(window_name='after threshold')
 
     original.open()
     after_proc.open()
-    while True:
-        ok, frame = camera.read()
-        frame = gbv.crop(frame, *bbox_bound)
-        frame = cv2.resize(frame, (800, 800), interpolation=interpol)
-        if not original.show_frame(frame):
-            break
-        if not after_proc.show_frame(frame):
-            break
 
-    original.close()
-    after_proc.close()
+    while True:
+        after_frame = imgp.get_frame()
+        original_frame = imgp.current_cropped_frame
+        if not original.show_frame(original_frame):
+            break
+        if not after_proc.show_frame(after_frame):
+            break
 
 
 if __name__ == '__main__':
