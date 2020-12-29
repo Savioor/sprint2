046257721896caf8a1081c6cab817095cb2abad6
taltatal <32167238+taltatal@users.noreply.github.com>
2020-12-29 18:05:00 +0200

    stuff

--- single_led_detector.py
+++ single_led_detector.py
@@ -1,10 +1,7 @@
 import cv2
-import numpy as np
 from reciever import SecretCamera
 
 import gbvision as gbv
-
-s = SecretCamera(SecretCamera.ARAZI)
 
 # ColorThreshold([[176, 255], [198, 255], [155, 255]], 'RGB')
 
@@ -12,13 +9,14 @@ s = SecretCamera(SecretCamera.ARAZI)
 
 ERODE_AND_DIALATE_DEFAULT = 30
 
+
 class ImageProcessing:
     INTERPOLATION = cv2.INTER_CUBIC
     STDV = 45
     WHITE = "WHITE"
     RED = "RED"
 
-    def __init__(self, camera, erode_and_dilate_size=ERODE_AND_DIALATE_DEFAULT, size=(1000,1000)):
+    def __init__(self, camera, erode_and_dilate_size=ERODE_AND_DIALATE_DEFAULT, size=(1000, 1000)):
         self.camera = camera
         self.camera_init()
         self.window = gbv.CameraWindow('feed', camera)
@@ -26,33 +24,34 @@ class ImageProcessing:
         self.erode_and_dialate_size = erode_and_dilate_size
         self.size = size
         self.pipelines = {ImageProcessing.WHITE: None, ImageProcessing.RED: None}
-        self.bbox_bound = None
+        self.bbox_bounds = [[], []]
         self.current_original_frame = None
-        self.current_cropped_frame = None
-        self.current_piped_frame = None
+        self.current_cropped_frames = [None, None]
+        self.current_piped_frame = [None, None]
 
     def setup(self):
         self.next_frame()
-        self.crop_init()
+        self.crop_init(0)
+        self.crop_init(1)
         self.init_pipeline(ImageProcessing.WHITE)
         self.init_pipeline(ImageProcessing.RED)
         cv2.destroyAllWindows()
 
     def camera_init(self):
-        #self.camera.set_exposure(-5)
-        #self.camera.resize(.1, .1)
+        # self.camera.set_exposure(-5)
+        # self.camera.resize(.1, .1)
         pass
 
-    def crop_init(self):
+    def crop_init(self, box_index):
         frame = self.get_original_frame()
-        self.bbox_bound = cv2.selectROI('feed', frame)
+        self.bbox_bounds[box_index] = cv2.selectROI('feed', frame)
 
     def init_pipeline(self, color):
         # pipeline = gbv.ColorThreshold([[0, 100], [0, 255], [0, 255]], 'HSV')
 
-        self.get_cropped_frame()
-        bbox = cv2.selectROI('feed', self.current_cropped_frame)
-        self.pipelines[color] = gbv.median_threshold(self.current_cropped_frame, ImageProcessing.STDV, bbox, 'RGB')
+        self.get_cropped_frames()
+        bbox = cv2.selectROI('feed', self.current_cropped_frames[0])
+        self.pipelines[color] = gbv.median_threshold(self.current_cropped_frames[0], ImageProcessing.STDV, bbox, 'RGB')
         print(self.pipelines[color])
         self.pipelines[color] += gbv.ErodeAndDilate(self.erode_and_dialate_size)
         return self.pipelines[color]
@@ -65,18 +64,23 @@ class ImageProcessing:
     def get_original_frame(self):
         return self.current_original_frame
 
-    def get_cropped_frame(self):
-        frame = gbv.crop(self.current_original_frame, *self.bbox_bound)
-        self.current_cropped_frame = cv2.resize(frame, self.size, interpolation=ImageProcessing.INTERPOLATION)
-        return self.current_cropped_frame
+    def get_cropped_frames(self):
+        cropped_frames = []
+        for i in range(len(self.bbox_bounds)):
+            frame = gbv.crop(self.current_original_frame, *self.bbox_bounds[i])
+            cropped_frames.append(cv2.resize(frame, self.size, interpolation=ImageProcessing.INTERPOLATION))
+        return self.current_cropped_frames
 
-    def get_piped_frame(self, color):
-        return self.pipelines[color](self.current_cropped_frame)
+    def get_piped_frames(self, color):
+        piped_frames = []
+        for frame in self.current_cropped_frames:
+            piped_frames.append(self.pipelines[color](frame))
 
     def get_frame(self, color):
         self.get_original_frame()
-        self.get_cropped_frame()
-        return self.get_piped_frame(color)
+        self.get_cropped_frames()
+        return self.get_piped_frames(color)
+
 
 class CircleFindWrapper:
     def __init__(self):
@@ -87,13 +91,13 @@ class CircleFindWrapper:
         l_red = self.circle_wrapper.find_shapes_unsorted(red_frame)
         return l_white, l_red
 
+
 def main():
-
+    # s = SecretCamera(SecretCamera.ARAZI)
     circle_finder = CircleFindWrapper()
-
-    #camera = gbv.USBCamera(0)
+    # camera = gbv.USBCamera(0)
     camera = gbv.USBCamera(r"C:\Users\t8854535\Desktop\sprint2\test_data\total_test3.avi")
-    #camera = s
+    # camera = s
     imgp = ImageProcessing(camera)
     imgp.setup()
 
