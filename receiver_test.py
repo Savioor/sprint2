9bba834c8cc4e52832fff51a9d8e16f3d9c03b18
Alexey Shapovalov <alexei.shapovalov.42@gmail.com>
2020-12-29 13:40:26 +0200

    Camera now gbcamera

---
+++ receiver_test.py
@@ -0,0 +1,15 @@
+import gbvision as gbv
+
+from reciever import SecretCamera
+
+
+def main():
+    camera = SecretCamera(SecretCamera.DANIEL)
+    window = gbv.RecordingCameraWindow(window_name='camera example', wrap_object=camera, file_name='record.avi',
+                                       fps=camera.get_fps())
+    window.show()
+    camera.release()
+
+
+if __name__ == '__main__':
+    main()
