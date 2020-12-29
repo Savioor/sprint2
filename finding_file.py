9babedb493ae62138b7fef17fa065fd573974976
Yotamor94 <oren.yotam@gmail.com>
2020-12-29 17:40:23 +0200

    Updated Arduino Module

--- finding_file.py
+++ finding_file.py
@@ -11,22 +11,20 @@ dl = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 drives = ['%s:' % d for d in dl if os.path.exists('%s:' % d)]
 
 
-def _check_usb():
+def check_usb():
     global drives
     while True:
         uncheckeddrives = ['%s:' % d for d in dl if os.path.exists('%s:' % d)]
         x = _diff(uncheckeddrives, drives)
         if x:
             return x[0]
-            foo()
-            drives = ['%s:' % d for d in dl if os.path.exists('%s:' % d)]
         x = _diff(drives, uncheckeddrives)
         if x:
-            ham()
             drives = ['%s:' % d for d in dl if os.path.exists('%s:' % d)]
 
 
-def find(path):
+def find():
+    path = check_usb()
     for root, dirs, files in os.walk(path):
         for file in files:
             if "txt" in file:
