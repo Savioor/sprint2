5580d2b2af4095d22ef4cee898e6c796af126954
taltatal <32167238+taltatal@users.noreply.github.com>
2020-12-29 15:05:41 +0200

    working sorta

--- single_led_detector.py
+++ single_led_detector.py
@@ -6,10 +6,14 @@ import gbvision as gbv
 
 s = SecretCamera(SecretCamera.DANIEL)
 
-stdv = np.array([20, 80, 80])
+# ColorThreshold([[176, 255], [198, 255], [155, 255]], 'RGB')
+
+stdv = np.array([5, 15, 15])
+stdv = np.array([50, 50, 50])
 
 def get_pipelined_window_stream(pipeline):
     return gbv.FeedWindow(window_name='after threshold', drawing_pipeline=pipeline)
+
 
 def show_pipelined_video(frame, pipeline):
     return get_pipelined_window_stream(pipeline).show_frame(frame)
@@ -19,29 +23,40 @@ def get_pipeline(*temp_args):
     # pipeline = gbv.ColorThreshold([[0, 100], [0, 255], [0, 255]], 'HSV')
     pipeline = gbv.median_threshold(*temp_args)
     print(pipeline)
-    #pipeline += gbv.ErodeAndDilate(2)
+    # pipeline += gbv.ErodeAndDilate(2)
     return pipeline
 
+def get_hidden_camera():
+    return
+
+
 def main():
-    camera = gbv.USBCamera(0)
-    #camera = gbv.USBCamera(r"C:\Users\t8854535\Desktop\sprint2\capture2.PNGrr")
+    # camera = gbv.USBCamera(0)
+    camera = gbv.USBCamera(r"C:\Users\t8854535\Desktop\sprint2\test_data\record.avi")
     camera.set_exposure(-5)
+    camera.resize(.1, .1)
     window = gbv.CameraWindow('feed', camera)
     window.open()
+
+    frame = window.show_and_get_frame()
+    bbox_bound = cv2.selectROI('feed', frame)
+
+    interpol = cv2.INTER_CUBIC
+
     while True:
-        frame = window.show_and_get_frame()
+        frame = gbv.crop(window.show_and_get_frame(), *bbox_bound)
+        frame = cv2.resize(frame, (1000, 1000), interpolation=interpol)
         k = window.last_key_pressed
         if k == 'r':
             bbox = None  # checks whole frame
+
             bbox = cv2.selectROI('feed', frame)
-            pipeline = get_pipeline(frame, stdv, bbox, 'HSV')
-            #thr = gbv.ColorThreshold([[0,100], [0,255], [0,255]], 'HSV')
+            pipeline = get_pipeline(frame, stdv, bbox, 'RGB')
+            # thr = gbv.ColorThreshold([[0,100], [0,255], [0,255]], 'HSV')
             break
     cv2.destroyAllWindows()
 
     print(pipeline)
-
-
 
     original = gbv.FeedWindow(window_name='original')
     after_proc = gbv.FeedWindow(window_name='after threshold', drawing_pipeline=pipeline)
@@ -50,6 +65,8 @@ def main():
     after_proc.open()
     while True:
         ok, frame = camera.read()
+        frame = gbv.crop(frame, *bbox_bound)
+        frame = cv2.resize(frame, (1000, 1000), interpolation=interpol)
         if not original.show_frame(frame):
             break
         if not after_proc.show_frame(frame):
